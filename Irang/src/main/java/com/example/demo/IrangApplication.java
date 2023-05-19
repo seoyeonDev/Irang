9855000c8caaa0f1da292c42cbc1aa76382ec90d
SkylarLee1003 <skylarlee1003@gmail.com>
2023-05-19 19:50:07 +0900

    Initial Setting

---
+++ Irang/src/main/java/com/example/demo/IrangApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrangApplication.class, args);
+	}
+
+}
